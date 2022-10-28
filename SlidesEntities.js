89891aa1da750b3932f2fcb5875f012874b67931
rakuzzan <100306099+rakuzzan@users.noreply.github.com>
2022-10-28 09:06:55 +0300

    Add files via upload

--- SlidesEntities.js
+++ SlidesEntities.js
@@ -1,8 +1,36 @@
+marker ={
+    choosenSlidesId: [2],
+    choosenElementsId: [1, 5, 3],
+    choosenTextId: []
+}
+
+vidSlides = {
+    savingHistory: []
+}
+
+presentation = {
+    nameDocument: 'First Presentation',
+    slides: [1, 2, 3, 4, 5]
+}
+
+slide ={
+    id: '3',
+    backgroundColor: 'white',
+    backgroundImg: null,
+    elementsId: [1, 2, 3, 4, 5]
+}
+
+element = {
+    id: '',
+    x: '',
+    y: ''
+}
+
 const img = {
     width: '200',
     height: '300',
-    x: '',
-    y: '',
+    x: '400',
+    y: '700',
     path: 'C:\\Users\\admin\\Downloads'
 }
 
@@ -18,4 +46,27 @@ const text = {
 const primitive = {
     type: 'circle',
     color: 'purple'
+}
+
+const triangle = {
+    firstX: '150',
+    firstY: '100',
+    secondX: '50',
+    secondY: '300',
+    thirdX: '250',
+    thirdY: '300',
+
+}
+
+const rectangle = {
+    x: '600',
+    y: '500',
+    width: '50',
+    height: '100'
+}
+
+const circle = {
+    x: '200',
+    y: '300',
+    radius: '10'
 }

79acc6a669b7ae213d31ed664d3aa03943e159be
rakuzzan <100306099+rakuzzan@users.noreply.github.com>
2022-12-30 09:31:04 +0300

    Update Circle.ts

--- lw3 front/Circle.ts
+++ lw3 front/Circle.ts
@@ -2,7 +2,7 @@ import { Types } from "./Type";
 import { Coordinates } from "./Coordinates";
 import { Props } from "./Props"
 
-export type Rectangle = {
+export type Circle = {
     type: Types;
     pos: Coordinates;
     radius: number;
